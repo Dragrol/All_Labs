df94e5ccbae3f6ef30beb70973be504a2f6d3ce0
Dragrol <dragrol@mail.ru>
2016-03-15 07:21:37 +0300

    Лабораторная №2 правки

--- lab_2/src/ru/bstu/iitus/vt41/pas/LabTwo.java
+++ lab_2/src/ru/bstu/iitus/vt41/pas/LabTwo.java
@@ -1,6 +1,8 @@
 package ru.bstu.iitus.vt41.pas;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by admin on 15.02.2016.
@@ -11,7 +13,13 @@ public class LabTwo {
         System.out.println("Введите путь к файлу");
         Scanner scanner = new Scanner(System.in);
         filePath = scanner.next();
-        EmailCheck emailCheck = new EmailCheck(filePath);
-        emailCheck.parseFile();
+        Pattern pattern = Pattern.compile("^[a-zA-Z]:(\\\\[a-zA-Z]+){1,10}\\.txt$");
+        Matcher matcher = pattern.matcher(filePath);
+        if(matcher.matches()) {
+            EmailCheck emailCheck = new EmailCheck(filePath);
+            emailCheck.parseFile();
+        }else {
+            System.out.println("Некорректный путь к файлу");
+        }
     }
 }
